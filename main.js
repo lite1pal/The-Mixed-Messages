dea80b85c4a6b5b49a2dd7cbccce9e7c2ebcff0c
lite1pal <deniskatarasenko6@gmail.com>
2023-01-16 12:39:05 +0200

    final commit

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log("Hello world");
+const words = {
+    adjectives: ['beautiful', 'tremendous', 'inspired', 'enermous', 'loud', 'dangerous', 'life-changing', 'small', 'far'],
+    nouns: ['bee', 'bridge', 'vampire', 'chair', 'cart', 'horse', 'sword', 'yacht', 'debt'],
+    verbs: ['tear', 'tremble', 'kick', 'float', 'speak', 'play', 'mount', 'race'],
+    adverbs: ['too', 'objectively', 'freakly speaking', 'just', 'usually', 'somehow', 'willingly']
+}
+
+const getRandomAdjective = (arr) => {
+    const adjective = words.adjectives[Math.floor(Math.random() * words.adjectives.length)]
+    return adjective.charAt().toUpperCase() + adjective.slice(1,);
+}
+
+const getRandomNoun = (arr) => {
+    return words.nouns[Math.floor(Math.random() * words.nouns.length)];
+}
+
+const getRandomVerb = (arr) => {
+    return words.verbs[Math.floor(Math.random() * words.verbs.length)];
+}
+
+const getRandomAdverb = (arr) => {
+    return words.adverbs[Math.floor(Math.random() * words.adverbs.length)];
+}
+
+const createRandomPhrase = () => {
+    console.log(getRandomAdjective() + " " + getRandomNoun() + " " + getRandomVerb() + "s" + " " + getRandomAdverb());
+}
+
+const futureDay = () => {
+    console.log(`Your day will be ${getRandomAdjective().toLowerCase()}`);
+}
+
+createRandomPhrase();
+futureDay();
